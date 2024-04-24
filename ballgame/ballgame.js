0fd9c3aec896c0af7b4d0dc4b1c4de3b5eb368c2
amber0dominguezz <163057811+amber0dominguezz@users.noreply.github.com>
2024-04-24 15:53:06 -0700

    Update ballgame.js

--- ballgame/ballgame.js
+++ ballgame/ballgame.js
@@ -32,6 +32,12 @@ function draw() {
   if (gameState=="L3"){
    levelThree(); 
   }
+  if (gameState=="L4"){
+    levelFour();
+  }
+  if (gameState=="L5"){
+    levelFive();
+  }
   
   text(("Score: " + score), width/2, 40);
   
@@ -67,10 +73,10 @@ function levelTwo(){
   if (distToBall <ballSize/2){
     ballx = random(width);
     bally = random(height);
-    ballSize = ballSize -2;
+    ballSize = ballSize -4;
     score = score +1;
   }
-  if(score>20){
+  if(score>15){
 
    gameState = "L3";
    
@@ -81,7 +87,7 @@ function levelTwo(){
 } // end level two
 
 function levelThree(){
-  background (200);
+  background (170);
   text("Level 3", width/2, height-20);
   var distToBall= dist(ballx, bally, mouseX, mouseY);
   if (distToBall <ballSize/2){
@@ -90,9 +96,48 @@ function levelThree(){
     score = score +1;
   }
   if(score>25){
+    
+    gameState = "L4";
 
   }
   
   ellipse(ballx, bally, ballSize, ballSize);
   image(capture, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
 } // end level three
+ 
+function levelFour(){
+  background (0);
+  text("Level 4", width/2, height-20);
+  fill('white');
+  var distToBall= dist(ballx, bally, mouseX, mouseY);
+  if (distToBall <ballSize/2){
+    ballx = random(width);
+    bally = random(height);
+    score = score +1;
+  }
+  if(score>35){
+    
+    gameState = "L5";
+
+  }
+  
+  ellipse(ballx, bally, ballSize, ballSize);
+  image(capture, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
+} // end level four
+
+function levelFive(){
+  background (random(255));
+  text("Level 5", width/2, height-20);
+  var distToBall= dist(ballx, bally, mouseX, mouseY);
+  if (distToBall <ballSize/2){
+    ballx = random(width);
+    bally = random(height);
+    score = score +1;
+  }
+  if(score>100){
+    
+  }
+  
+  ellipse(ballx, bally, ballSize, ballSize);
+  image(capture, ballx-ballSize/2, bally-ballSize/2, ballSize, ballSize);
+} // end level five
